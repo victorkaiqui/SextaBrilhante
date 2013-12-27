fc31e11fd17b17f7b8d691f298283d0979ba9da8
vpaz <vpaz@ICI-88149.ICI>
2013-12-27 11:10:57 -0200

    Comunicação serial

--- src/br/com/desafios/main/Main.java
+++ src/br/com/desafios/main/Main.java
@@ -1,21 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.desafios.main;
 
-import br.com.desafios.projectEuler.*;
-import br.com.desafios.sextaBrilhante.*;
-
-/**
- *
- * @author @victorkaiqui
- */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        ProjectEuler41.pandigitalPrime();
-     
     }
 }
